7bd8abb7eff046598dc35b79e299ff0b4a3acced
Paradox <maazkashif492@outlook.com>
2020-12-14 14:04:30 +0500

    Initial Upload

---
+++ InfiniteStamina/InfiniteStamina/Main.cs
@@ -0,0 +1,35 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteStamina
+{
+    public class Main : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log("InfiniteStamina has been loaded");
+
+            UnturnedPlayerEvents.OnPlayerUpdateStamina += OnStaminaUpdated;
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("InfiniteStamina has been unloaded");
+
+            UnturnedPlayerEvents.OnPlayerUpdateStamina -= OnStaminaUpdated;
+        }
+
+        private void OnStaminaUpdated(UnturnedPlayer player, byte stamina)
+        {
+            if (stamina != 100)
+                player.Player.life.serverModifyStamina(100);
+        }
+    }
+}
